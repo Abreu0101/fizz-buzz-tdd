eff71edc0a2b54064efad22ba542c4a511fec2ea
Jose <jose@medici.md>
2020-09-07 08:55:57 -0400

    Initialize Project

---
+++ FizzBuzz TDD/FizzBuzz_TDD.h
@@ -0,0 +1,19 @@
+//
+//  FizzBuzz_TDD.h
+//  FizzBuzz TDD
+//
+//  Created by Roberto Abreu on 9/7/20.
+//  Copyright © 2020 Roberto Abreu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FizzBuzz_TDD.
+FOUNDATION_EXPORT double FizzBuzz_TDDVersionNumber;
+
+//! Project version string for FizzBuzz_TDD.
+FOUNDATION_EXPORT const unsigned char FizzBuzz_TDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizzBuzz_TDD/PublicHeader.h>
+
+
